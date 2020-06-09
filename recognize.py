b4bf909289831f1dceace125dfc4aebfc1f04594
Manikantha M <62506185+ManikanthaM@users.noreply.github.com>
2020-06-09 10:14:01 +0530

    Update recognize.py

--- recognize.py
+++ recognize.py
@@ -2,7 +2,7 @@
 # python recognize.py --detector face_detection_model \
 #	--embedding-model openface_nn4.small2.v1.t7 \
 #	--recognizer output/recognizer.pickle \
-#	--le output/le.pickle --image images/adrian.jpg
+#	--le output/le.pickle --image images/Mani.jpg
 
 # import the necessary packages
 import numpy as np
